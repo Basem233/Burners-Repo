99f8747641e00d08a2dfb4b5dc8e8af7ed7d93a2
Basem75 <basem.fahem23@gmail.com>
2023-10-26 19:27:04 +0300

    Editing the Driver

--- CLCD/CLCD_interface.h
+++ CLCD/CLCD_interface.h
@@ -15,28 +15,20 @@ typedef struct
 	u8 Copy_u8YPos;
 }CURSOR_LOC;
 
-typedef struct 
-{
-	u8* Copy_u8Arr;
-	u8 Copy_u8BlockNumber;
-	u8 Copy_u8XPos;
-	u8 Copy_u8YPos;
-}Write_SPEC;
 
-void CLCD_voidSendCommand(u8 Copy_u8Command);
+u8 HAL_CLCD_u8SendCommand(u8 Copy_u8Command);
 
-void CLCD_voidSendData(u8 Copy_u8Data);
+u8 HAL_CLCD_u8SendData(u8 Copy_u8Data);
 
-void CLCD_voidInit(void);
+u8 HAL_CLCD_u8Init(void);
 
 /*Ay haga hthsel askii value yb2a lazm tkon char*/
-void CLCD_voidSendString(const char* Copy_pcString);
+u8 HAL_CLCD_u8SendString(const char* Copy_pcString);
 
-void CLCD_voidGoToXY(CURSOR_LOC * ptr);
+u8 HAL_CLCD_u8GoToXY(CURSOR_LOC * ptr);
 
-void CLCD_voidWriteSpecialCharacter(Write_SPEC * ptr);
 
-void CLCD_voidWriteNumber(u32 Copy_u32Number);
+u8 HAL_CLCD_u8_ClearDisplay(void);
 
 
 
--- CLCD/CLCD_program.c
+++ CLCD/CLCD_program.c
@@ -8,142 +8,174 @@
 #include "CLCD_config.h"
 #include "CLCD_private.h"
 
+PIN_CONFIG RS_LOW = {CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8PIN_LOW};
+PIN_CONFIG RS_HIGH = {CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8PIN_HIGH};
 
-void CLCD_voidInit(void)
+PIN_CONFIG RW_LOW = {CLCD_CONTROL_PORT,CLCD_RW_PIN,DIO_u8PIN_LOW};
+PIN_CONFIG RW_HIGH = {CLCD_CONTROL_PORT,CLCD_RW_PIN,DIO_u8PIN_HIGH};
+
+PIN_CONFIG EN_LOW = {CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_LOW};
+PIN_CONFIG EN_HIGH = {CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_HIGH};
+
+
+
+u8 HAL_CLCD_u8Init(void)
 {
+	u8 Local_u8ErrorState=OK;
+
 	#if CLCD_MODE == CLCD_8BIT_MODE
 	/*1-Wait more than 30 ms*/
 	_delay_ms(40);
 
 	/*Function set Command : 2 Lines (1), 5*8 Font size (0)*/
-	CLCD_voidSendCommand(0b00111000);
+	HAL_CLCD_u8SendCommand(0b00111000);
 
 	/*Display on/off control:Enable display(1),Disable cursor(0),no Blink cursur(0)*/
-	CLCD_voidSendCommand(0b00001100);
+	HAL_CLCD_u8SendCommand(0b00001100);
 
 	/*Clear display*/
-	CLCD_voidSendCommand(1);//0b00000001
+	HAL_CLCD_u8SendCommand(1);//0b00000001
 
 #elif CLCD_MODE == CLCD_4BIT_MODE
 	_delay_ms(40);
 	/*Set 4 bit mode*/
-	CLCD_voidSendCommand(0b0010);
+	HAL_CLCD_u8SendCommand(0b0010);
 	/*Function Set*/
-	CLCD_voidSendCommand(0b00101000);
+	HAL_CLCD_u8SendCommand(0b00101000);
 	_delay_us(45);
 	//set display on/off control
-	CLCD_voidSendCommand(0b00001111);
+	HAL_CLCD_u8SendCommand(0b00001111);
 
 	_delay_us(45);
 	//set display clear
-	CLCD_voidSendCommand(0b00000001);
+	HAL_CLCD_u8SendCommand(0b00000001);
 	_delay_ms(2);
 	//set entry mode
-	CLCD_voidSendCommand(0b00000110);
+	HAL_CLCD_u8SendCommand(0b00000110);
 
 #endif
+
+	return Local_u8ErrorState;
 }
 
 
-void CLCD_voidSendCommand(u8 Copy_u8Command)
+u8 HAL_CLCD_u8SendCommand(u8 Copy_u8Command)
 {
+	u8 Local_u8ErrorState=OK;
+
 	#if CLCD_MODE == CLCD_8BIT_MODE
 	/*SET RS Pin to LOW For command*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(&RS_LOW);
 
 	/*SET RW Pin to LOW To Write*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_RW_PIN,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(&RW_LOW);
 
+
+	PORT_CONFIG PORT_COM = {CLCD_DATA_PORT,Copy_u8Command};
 	/*Set Command to Data Pins*/
-	DIO_u8SetPortValue(CLCD_DATA_PORT,Copy_u8Command);
+	DIO_u8SetPortValue(&PORT_COM);
 
 	/*Send Enable Pulse*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_HIGH);
+	DIO_u8SetPinValue(&EN_HIGH);
 	_delay_ms(2);
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(&EN_LOW);
 
 #elif CLCD_MODE == CLCD_4BIT_MODE
 
 	/*SET RS Pin to LOW For command*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(&RS_LOW);
 
 	/*Send Higher data*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_HIGH);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_HIGH);
+	_delay_ms(9);
 	u8 datah=((Copy_u8Command >>4)&0x07) | (((Copy_u8Command >>4)&(0x08))<<1);
-	DIO_u8SetPortValue(CLCD_DATA_PORT,datah);
+	PORT_CONFIG Send_datah ={CLCD_DATA_PORT,datah};
+	DIO_u8SetPortValue(&Send_datah);
 	/*Send Enable Pulse*/
 
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_LOW);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_LOW);
+	_delay_ms(9);
 
 	/*Send Lower data*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_HIGH);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_HIGH);
+	_delay_ms(9);
 	u8 datal=(Copy_u8Command & 0x07) | (((Copy_u8Command)&(0x08))<<1);
-	DIO_u8SetPortValue(CLCD_DATA_PORT,datal);
+	PORT_CONFIG Send_datal ={CLCD_DATA_PORT,datal};
+	DIO_u8SetPortValue(&Send_datal);
 	/*Send Enable Pulse*/
 
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_LOW);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_LOW);
+	_delay_ms(9);
 
 #endif
+
+	return Local_u8ErrorState;
 }
 
-void CLCD_voidSendData(u8 Copy_u8Data)
+u8 HAL_CLCD_u8SendData(u8 Copy_u8Data)
 {
+	u8 Local_u8ErrorState = OK;
 	#if CLCD_MODE == CLCD_8BIT_MODE
 	/*SET RS Pin to HIGH For Data*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8PIN_HIGH);
+	DIO_u8SetPinValue(&RS_HIGH);
 
 	/*SET RW Pin to LOW To Write*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_RW_PIN,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(&RW_LOW);
 
+
+	PORT_CONFIG PORT_DATA = {CLCD_DATA_PORT,Copy_u8Data};
 	/*Set Data to Data Pins*/
-	DIO_u8SetPortValue(CLCD_DATA_PORT,Copy_u8Data);
+	DIO_u8SetPortValue(&PORT_DATA);
 
 	/*Send Enable Pulse*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_HIGH);
+	DIO_u8SetPinValue(EN_HIGH);
 	_delay_ms(2);
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_LOW);
+	DIO_u8SetPinValue(EN_LOW);
 
 #elif CLCD_MODE == CLCD_4BIT_MODE
 
+
 	/*SET RS Pin to HIGH For Data*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_RS_PIN,DIO_u8PIN_HIGH);
+	DIO_u8SetPinValue(&RS_HIGH);
 
 	/*Send Higher data*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_HIGH);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_HIGH);
+	_delay_ms(9);
 	u8 datah=((Copy_u8Data >>4)&0x07) | (((Copy_u8Data >>4)&(0x08))<<1);
-	DIO_u8SetPortValue(CLCD_DATA_PORT,datah);
+	PORT_CONFIG Send_datah ={CLCD_DATA_PORT,datah};
+	DIO_u8SetPortValue(&Send_datah);
 	/*Send Enable Pulse*/
 
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_LOW);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_LOW);
+	_delay_ms(9);
 
 	/*Send Lower data*/
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_HIGH);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_HIGH);
+	_delay_ms(9);
 	u8 datal=(Copy_u8Data & 0x07) | (((Copy_u8Data)&(0x08))<<1);
-	DIO_u8SetPortValue(CLCD_DATA_PORT,datal);
+	PORT_CONFIG Send_datal ={CLCD_DATA_PORT,datal};
+	DIO_u8SetPortValue(&Send_datal);
 	/*Send Enable Pulse*/
 
-	DIO_u8SetPinValue(CLCD_CONTROL_PORT,CLCD_E_PIN,DIO_u8PIN_LOW);
-	_delay_ms(4);
+	DIO_u8SetPinValue(&EN_LOW);
+	_delay_ms(9);
 
 #endif
+
+	return Local_u8ErrorState;
 }
 
 
 /*Ay haga hthsel askii value yb2a lazm tkon char*/
-void CLCD_voidSendString(const char* Copy_pcString)
+u8 HAL_CLCD_u8SendString(const char* Copy_pcString)
 {
+	u8 Local_u8ErrorState=OK;
+
 	#if CLCD_MODE == CLCD_8BIT_MODE
 	u8 Local_u8Counter = 0;
 	while (Copy_pcString[Local_u8Counter] !='\0')
 	{
-		CLCD_voidSendData(Copy_pcString[Local_u8Counter]);
+		HAL_CLCD_u8SendData(Copy_pcString[Local_u8Counter]);
 		Local_u8Counter++;
 	}
 
@@ -151,12 +183,15 @@ void CLCD_voidSendString(const char* Copy_pcString)
 	u8 Local_u8Counter = 0;
 	while (Copy_pcString[Local_u8Counter] !='\0')
 	{
-		CLCD_voidSendData(Copy_pcString[Local_u8Counter]);
+		HAL_CLCD_u8SendData(Copy_pcString[Local_u8Counter]);
 		Local_u8Counter++;
 	}
+#endif
+
+	return Local_u8ErrorState;
 }
 
-void CLCD_voidWriteNumber(u32 Copy_u32Number)
+void HAL_CLCD_u8WriteNumber(u32 Copy_u32Number)
 {
 	u8 arr[10]="0123456789";//To get the askii value
 	u8 LoopCounter;
@@ -178,8 +213,17 @@ void CLCD_voidWriteNumber(u32 Copy_u32Number)
 	/*b2sm el rkm w bb3t l askii bta3o*/
 	for (LoopCounter=counter;LoopCounter>0;LoopCounter--)
 	{
-		CLCD_voidSendData(arr[(number /i)%10]);
+		HAL_CLCD_u8SendData(arr[(number /i)%10]);
 		i=i/10;
 	}
+}
+
+u8 HAL_CLCD_u8_ClearDisplay(void)
+{
+	u8 Local_u8ErrorState =OK;
+
+	HAL_CLCD_u8SendCommand(1);
+
+	return Local_u8ErrorState;
 }
 
