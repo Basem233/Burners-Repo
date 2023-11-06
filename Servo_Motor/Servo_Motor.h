1d74527bac502324a4ed14099139d9f4d0981b41
Hazem-murshedi <107986127+Hazem-murshedi@users.noreply.github.com>
2023-11-06 02:13:49 +0200

    Update Servo_Motor.h

--- Servo_Motor/Servo_Motor.h
+++ Servo_Motor/Servo_Motor.h
@@ -1,24 +1,17 @@
-/****************************************************************************************************************
- * File Name: DC_Motor.h
- * Date: 4/8/2023
- * Driver: DC Motor Driver Header File
- * Author: Youssef Zaki
- ****************************************************************************************************************/
-
 
 
 #ifndef SERVO_MOTOR_H_
 #define SERVO_MOTOR_H_
 
 #include "STD_TYPES.h"
-
+#include "DIO_interface.h"
 /******************************************************************************************
  *                                    Macros Definitions                                  *
  ******************************************************************************************/
 
-#define SERVO_MOTOR_DIR_ID                  DDRD
-#define SERVO_MOTOR_PORT_ID                 PORTD
-#define SERVO_MOTOR_PIN_ID                  5
+#define SERVO_MOTOR_DIR_ID                  DDRB
+#define SERVO_MOTOR_PORT_ID                 DIO_PORTB
+#define SERVO_MOTOR_PIN_ID                  DIO_PIN3
 
 
 
@@ -39,14 +32,13 @@ typedef enum {
  * The Function responsible for setup the direction for the SERVO motor pin through the DIO driver.
  * Initialization of Timer0.
  */
-void ServoMotor_Init(void);
+u8 ServoMotor_Init(PIN_CONFIG * ptr);
 
 /*
  * DESCRIPTION:
  * The function responsible for set the angle of the Servo Motor  based on the state
  * input state value.
  */
-void ServoMotor_Rotate(ServoMotor_State state);
-
+void ServoMotor_Rotate(PIN_CONFIG * ptr,ServoMotor_State state);
 
 #endif
